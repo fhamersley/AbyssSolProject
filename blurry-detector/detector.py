3e5228bbf45b23bfe7901628259c20cf142c4241
fhamersley <fhamersley@gmail.com>
2018-10-09 21:48:30 +1100

    Basic threshold using variance of laplacian

--- blurry-detector/detector.py
+++ blurry-detector/detector.py
@@ -8,21 +8,26 @@ def image_rescale(image, scale):
     """
     Rescale a given image using a given scale
     """
-    (h, w) = image.shape[:2]    # Input image size
+    # Input image size
+    (H, W) = image.shape[:2]
 
-    outh = round(h * scale, 0)  # Rescale height and width
-    outw = round(w * scale, 0)
-    dim = int(outw), int(outh)       # Output dimension
+    # Rescale height and width
+    outH = round(H * scale, 0)      
+    outW = round(W * scale, 0)
 
-    rescaled = cv2.resize(image, dim, cv2.INTER_AREA) # Rescale
+    # Output dimension
+    dim = int(outW), int(outH)
 
+    # Rescale and return
+    rescaled = cv2.resize(image, dim, cv2.INTER_AREA)
     return rescaled
 
-
+# Not enough input arguments, exit
 if len(sys.argv) == 1:
     print "Not enough input arguments. Please input an image"
     sys.exit()
 
+# Too many input arguments, exit
 if len(sys.argv) > 2:
     print "Too many input arguments. Please input only the image"
     sys.exit()
@@ -30,6 +35,29 @@ if len(sys.argv) > 2:
 image = cv2.imread(sys.argv[1])
 #image = cv2.imread('../blurry-data/b1.JPG')
 
+# Check image has been loaded
+if image is None:
+    print ('Error in opening image')
+    sys.exit()
+
+# Rescale the image to 20% of original size
 resImage = image_rescale(image, 0.2)
-cv2.imshow("Image", resImage)
+
+# Convert to grayscale
+grayImage = cv2.cvtColor(resImage, cv2.COLOR_BGR2GRAY)
+
+# Compute the laplacian with default 3x3 kernel
+lapImage = cv2.Laplacian(grayImage, cv2.CV_64F)
+
+# Compute the variance of the laplacian
+imVar = lapImage.var()
+print(imVar)
+
+# Decide if faulty or not - need to tune threshold
+if imVar > 500:
+    print(0)
+else:
+    print(1)
+
+cv2.imshow("Image", grayImage)
 cv2.waitKey(0)
